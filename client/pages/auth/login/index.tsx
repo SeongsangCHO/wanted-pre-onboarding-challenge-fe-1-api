19e684e00877fc43d574a0fa6a698497243afd9d
SeongsangCHO <liasqui31@gmail.com>
2023-01-08 15:49:47 +0900

    feat: LoginPage-form 퍼블리싱

--- client/pages/auth/login/index.tsx
+++ client/pages/auth/login/index.tsx
@@ -2,31 +2,60 @@ import React from "react";
 
 interface IndexProps {}
 
+const REGEX = {
+  email: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+};
+
 const LoginPage = ({}: IndexProps) => {
-  const handleSubmitLoginForm = () => {};
+  const [loginValidation, setLoginValidation] = React.useState({
+    email: false,
+    password: false,
+  });
+  const [formInputs, setFormInputs] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmitLoginForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value: emailInputValue } = e.target;
+    const isEmailValid = REGEX.email.test(emailInputValue);
+    setLoginValidation((p) => ({ ...p, email: isEmailValid }));
+    setFormInputs((p) => ({ ...p, email: e.target.value }));
+  };
+
+  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value: passwordInputValue } = e.target;
+    const isPasswordValid = passwordInputValue.length >= 8;
+    setLoginValidation((p) => ({ ...p, password: isPasswordValid }));
+    setFormInputs((p) => ({ ...p, password: e.target.value }));
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmitLoginForm}>
         <label>
           Id
-          <input name="" 
-          pattern="
-          //최소 @와 .을 포함해야함 - 이메일정규식
-          "/>
+          <input
+            name="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+            value={formInputs.email}
+            onChange={handleChangeEmail}
+          />
         </label>
         <label>
           Password
-          <input 
-          minLength={8}
-          //최소 8자이상입력해야함
+          <input
+            name="password"
+            value={formInputs.password}
+            onChange={handleChangePassword}
+            minLength={8}
           />
         </label>
-        <button
-          disabled={
-            false
-            // "이메일 비밀번호가 모두 입력이 되어있고 조건을 만족해야 활성화"
-          }
-        >
+        <button disabled={!(loginValidation.email && loginValidation.password)}>
           Login
         </button>
       </form>
